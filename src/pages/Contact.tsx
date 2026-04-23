8c2b03953686455aec98d5eafe92645f9bebc2e5
mekdesesayas302-dev <mekdesesayas302@gmail.com>
2026-04-23 04:38:39 -0700

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -56,7 +56,7 @@ const Contact = () => {
 
     try {
       setSending(true);
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://dilla-library-backend.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
